94ab80ceda1b52edc6c831e63d37195521afe27b
thefomin <raveyt@yandex.ru>
2025-09-23 19:19:32 +0700

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+  await app.listen(config.getOrThrow<string>('APPLICATION_PORT'), '0.0.0.0');
+}
+bootstrap();
