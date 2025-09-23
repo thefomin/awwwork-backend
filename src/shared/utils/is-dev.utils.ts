14db2eed30a3c94d43d5557bb3a17f5055855905
thefomin <raveyt@yandex.ru>
2025-09-24 00:07:16 +0700

    del: test const delete

--- src/shared/utils/is-dev.utils.ts
+++ src/shared/utils/is-dev.utils.ts
@@ -1,15 +1,9 @@
 import { ConfigService } from '@nestjs/config'
 import * as dotenv from 'dotenv'
 
-import { AuthService } from '@/features/auth/auth.service'
-
 dotenv.config()
 
 export const isDev = (configService: ConfigService) =>
 	configService.getOrThrow('NODE_ENV') === 'development'
 
 export const IS_DEV_ENV = process.env.NODE_ENV === 'development'
-
-export const test = () => {
-	const {} = new AuthService()
-}
