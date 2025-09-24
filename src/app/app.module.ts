93fbc9af0aaebd673c9977dc2ac4a67eae64cfdd
thefomin <raveyt@yandex.ru>
2025-09-24 12:45:34 +0700

    feat(main): modules added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
+import { ApiModule } from '@/shared/api/api.module'
 import { IS_DEV_ENV } from '@/shared/utils'
+
+import { FeaturesModule } from '@/features'
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			ignoreEnvFile: !IS_DEV_ENV,
 			isGlobal: true
-		})
+		}),
+		FeaturesModule,
+		ApiModule
 	],
 	controllers: [],
 	providers: []
