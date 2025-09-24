d61e82528d6ea6b90f4f4d6147b5fa35a1ec1408
thefomin <raveyt@yandex.ru>
2025-09-24 12:46:05 +0700

    feat: swagger config added

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,11 +1,24 @@
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	const config = app.get(ConfigService)
+	const swaggerConfig = new DocumentBuilder()
+		.setTitle('Awwwork API')
+		.setDescription('API Documentation')
+		.setVersion('1.0.0')
+		.setContact('Awwwork', 'https://awwwork.ru', 'info@awwwork.ru')
+		.build()
+
+	const document = SwaggerModule.createDocument(app, swaggerConfig)
+
+	SwaggerModule.setup('/docs', app, document, {
+		yamlDocumentUrl: '/swagger.yaml'
+	})
 	await app.listen(config.getOrThrow<string>('APPLICATION_PORT'), '0.0.0.0')
 }
 bootstrap().catch(err => {
