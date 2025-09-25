c211c69a616374b20664dfa0c2c0cc19de453192
thefomin <raveyt@yandex.ru>
2025-09-25 15:28:10 +0700

    feat(features): users module added

--- src/features/features.module.ts
+++ src/features/features.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { ScheduleModule } from '@nestjs/schedule'
 
 import { CredentialsModule } from './auth/credentials/credentials.module'
+import { UsersModule } from './users/users.module'
 
 @Module({
-	imports: [ScheduleModule.forRoot(), CredentialsModule],
+	imports: [ScheduleModule.forRoot(), CredentialsModule, UsersModule],
 	providers: []
 })
 export class FeaturesModule {}
