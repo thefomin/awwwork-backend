bedd1dbb24a18ab6eac9b5996946c8c2cc5671d9
thefomin <raveyt@yandex.ru>
2025-09-27 15:10:22 +0200

    feat(features): profile module added

--- src/features/features.module.ts
+++ src/features/features.module.ts
@@ -4,12 +4,14 @@ import { ScheduleModule } from '@nestjs/schedule'
 import { CredentialsModule } from './auth/credentials/credentials.module'
 import { CareersModule } from './careers/careers.module'
 import { UsersModule } from './users/users.module'
+import { ProfilesModule } from './profiles/profiles.module'
 
 @Module({
 	imports: [
 		ScheduleModule.forRoot(),
 		CredentialsModule,
 		UsersModule,
+		ProfilesModule,
 		CareersModule
 	],
 	providers: []
